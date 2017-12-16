3e34fd7e6bc1c759608ab5171d686f6b1e2d4de5
Salomone Baquis <salomone.l.baquis@gmail.com>
2017-12-16 17:11:57 -0500

    stuff

--- index.js
+++ index.js
@@ -1,115 +1,112 @@
 var addButton = document.querySelector(".add")
 var submitButton = document.querySelector("button[type='submit']")
 var memberList = document.querySelector(".household")
+var householdMembers = [] || householdMembers
 
 // Sets button type so all buttons don't reset or submit form
 var buttons = document.querySelectorAll("button")
 buttons.forEach(function(button) {
 	button.setAttribute("type", "button")
 })
-
-// Where we'll store our household members
-var householdMembers = [] || householdMembers
-
 // Function to create a household member
 function HouseholdMember(values) {
 	this.age = values[0]
 	this.relationship = values[1]
 	this.smoker = values[2]
 }
-
 // Method to set a pseudo-unique four digit household member ID  
 HouseholdMember.prototype.setId = function() {
-	this.id = createId()
-
-	function createId() {
+	this.id = (function() {
 		var uniqueId = Math.floor((Math.random() * 9000) + 1000)
 		householdMembers.forEach(function(member) {
 			if (member.id === uniqueId) {
 				createId()
 			} 
 		})
-		return uniqueId
-	}
-} 
-
-// Form button events
-addButton.addEventListener("click", createHouseholdMember) 
-submitButton.addEventListener("click", submitHouseholdList) 
+		return uniqueId	
+	})()
+}
 
 // When we submit our JSON household list
 function submitHouseholdList() {
-	var debugElement = document.querySelector(".debug")
-	debugElement.innerHTML = ""
-	debugElement.style.cssText = "color:red; display:block;"
-	var submissionErrorText = document.createTextNode("Please add at least one household member.")
-	householdMembers.length ? jsonifyHouseholdMembers(householdMembers, debugElement) : debugElement.appendChild(submissionErrorText)
+	clearErrors()
+	householdMembers.length ? jsonifyList(householdMembers) : createSubmissionError()
+
+	function jsonifyList(memberList) {
+		var debugElement = document.querySelector(".debug")
+		debugElement.innerHTML = ""	
+		debugElement.style.cssText = "display:block;"
+		var jsonMembers = JSON.stringify({memberList}, null, 2)
+		jsonMembers = document.createTextNode(jsonMembers)
+		debugElement.appendChild(jsonMembers)
+	}
+
+	function createSubmissionError() {
+		var builderElement = document.querySelector(".builder")
+		var submissionError = createElement("p", "Please add at least one household member.", "error")
+		builderElement.appendChild(submissionError)
+	}	
 }
 
-function jsonifyHouseholdMembers(memberList, debugElement) {
-	var jsonMembers = JSON.stringify({memberList}, null, 2)
-	jsonMembers = document.createTextNode(jsonMembers)
-	debugElement.appendChild(jsonMembers)
-}
-
-// Initializes member and adds to submission list
 function createHouseholdMember() {
-	var errors = document.querySelectorAll(".error")
-	clearErrors(errors)
-	var fields = document.querySelectorAll("[name]")
+	clearErrors()
 	var fieldValues = [] || fieldValues
 	// If form values pass our tests
-	if (testFields(fields, fieldValues)) {		
-		var member = new HouseholdMember(fieldValues)
-		initializeMember(member)
+	if (testFields(fieldValues)) {		
+		initializeMember(fieldValues)
 		document.querySelector("form").reset()
 	}
 }
 
-// Sets member ID, adds to array for submission, and inserts into DOM
-function initializeMember(member) {
+// When the member is created
+function initializeMember(fieldValues) {
+	var member = new HouseholdMember(fieldValues)
 	member.setId()
 	householdMembers.push(member)
 	addMemberToList(member)
 }
 
-// Adds member to the DOM list
+// Adds member to the DOM
 function addMemberToList(member) {
-	var memberEntry = document.createElement("li")
-	memberEntry.className = "household-member"
+	var memberEntry = createElement("li", createHouseholdMemberListEntry(member), "household-member")
 	memberEntry.setAttribute("data-value", member.id)
-	var memberText = document.createTextNode(createHouseholdMemberListEntry(member))
-
-	// Remove button
-	var removeButton = document.createElement("button")
-	removeButton.className = "remove"
-	var removeButtonLabel = document.createTextNode("Remove")
-	removeButton.appendChild(removeButtonLabel)	
-
-	memberEntry.appendChild(memberText)
+	var removeButton = createElement("button", "Remove", "remove")
 	memberEntry.appendChild(removeButton)
 	memberList.appendChild(memberEntry)
 
 	removeButton.addEventListener("click", function() {
 		removeMember(member)
+		console.log(householdMembers)
 	})
+}
+
+function createElement(tag, text, className) {
+	var element = document.createElement(tag)
+	if (className) {
+		element.className = className		
+	}
+	var elementText = document.createTextNode(text)
+	element.appendChild(elementText)
+	return element
 }
 
 // Removes a member from the DOM and the submission list
 function removeMember(member) {
-	var memberListEntries = document.querySelectorAll(".household-member")
-	for (var i = 0; i < householdMembers.length && i < memberListEntries.length; i++) {
-		var listId = memberListEntries[i].getAttribute("data-value")
+	var domListEntry = document.querySelectorAll(".household-member")
+
+	for (var i = 0; i < householdMembers.length && i < domListEntry.length; i++) {
+		var listId = domListEntry[i].getAttribute("data-value")
 		var arrayId = householdMembers[i].id
 		if (listId == member.id && arrayId === member.id) {
-			memberListEntries[i].remove()
+			domListEntry[i].remove()
 			householdMembers.splice([i], 1)
 		}
 	}
 }
 
 // Validate form entries
-function testFields(fields, valuesArray) {
+function testFields(valuesArray) {
+	var fields = document.querySelectorAll("[name]")
 	var bool = true
 	for (i = 0; i < fields.length; i++) {
 		var field = fields[i]
@@ -119,7 +116,7 @@ function testFields(fields, valuesArray) {
 			case "age": 
 				var value = Number(value)
 				if (!value || isNaN(value) === true || value < 1) { 
-					createErrorMessage(field)
+					formErrorMessage(field)
 					bool = false				
 				} else {
 					valuesArray.push(value)
@@ -127,7 +124,7 @@ function testFields(fields, valuesArray) {
 				break;
 			case "rel":
 				if (!value) { 					
-					createErrorMessage(field)
+					formErrorMessage(field)
 					bool = false					
 				}	else {
 					valuesArray.push(value)
@@ -143,7 +140,7 @@ function testFields(fields, valuesArray) {
 }
 
 // Create error message
-function createErrorMessage(formField) {
+function formErrorMessage(formField) {
 	var errorElement = document.createElement("p")
 	errorElement.className = "error"
 	formField.style.outline = "1px solid red"
@@ -158,7 +155,8 @@ function createErrorMessage(formField) {
 }
 
 // Clear error messages
-function clearErrors(errors) {
+function clearErrors() {
+	var errors = document.querySelectorAll(".error")
 	for (var i = 0; i < errors.length; i++) {
 		errors[i].tagName === "P" ? errors[i].remove() : errors[i].removeAttribute("style")
 	}
@@ -177,3 +175,7 @@ function createHouseholdMemberListEntry(member) {
 function capitalizeFirstLetter(string) {
 	return typeof string === "string" ? string.charAt(0).toUpperCase() + string.slice(1) : string
 }
+
+// Button events
+addButton.onclick = createHouseholdMember
+submitButton.onclick = submitHouseholdList
